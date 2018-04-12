68fd18e44d7c6b384612dddafb94d145a9263118
kaushalsharma311 <38313525+kaushalsharma311@users.noreply.github.com>
2018-04-12 18:53:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ def hello_world():
   return '''
 <html>
     <head>
-        <title>Home Page - Microblog</title>
+        <title>Dashboard</title>
     </head>
     <body>
         <iframe width="800" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiODM2YTE1ZGYtZmMzYS00YWMyLWFkNjYtNTliOTNkYjhkODRjIiwidCI6ImRhNjdlZjFiLWNhNTktNGRiMi05YThjLWFhOGQ5NDYxN2ExNiIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+        <form method="POST">
+            <input class="btn" type="submit" value="submit">
+        </form>
     </body>
 </html>'''
 
