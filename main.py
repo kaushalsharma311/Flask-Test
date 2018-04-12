c86d616018fa6c460c7410a198927695a46c8a40
kaushalsharma311 <38313525+kaushalsharma311@users.noreply.github.com>
2018-04-12 14:15:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, Kaushal!'
+  return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <iframe width="800" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiODM2YTE1ZGYtZmMzYS00YWMyLWFkNjYtNTliOTNkYjhkODRjIiwidCI6ImRhNjdlZjFiLWNhNTktNGRiMi05YThjLWFhOGQ5NDYxN2ExNiIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+    </body>
+</html>'''
 
 if __name__ == '__main__':
   app.run()
