542667b58cf7bd90af609755d0d8023926c585f3
kaushalsharma311 <38313525+kaushalsharma311@users.noreply.github.com>
2018-04-13 13:04:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def hello_world():
         <title>Dashboard</title>
     </head>
     <body>
-        <iframe width="800" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiODM2YTE1ZGYtZmMzYS00YWMyLWFkNjYtNTliOTNkYjhkODRjIiwidCI6ImRhNjdlZjFiLWNhNTktNGRiMi05YThjLWFhOGQ5NDYxN2ExNiIsImMiOjR9/ReportSection" frameborder="0" allowFullScreen="true"></iframe>
+        <iframe width="800" height="600" src="https://app.powerbi.com/groups/me/reports/76c60a26-95fc-4126-9d86-536e7fc7bf9e/ReportSection" frameborder="0" allowFullScreen="true"></iframe>
         <form method="POST">
             <input class="btn" type="submit" value="submit">
         </form>
