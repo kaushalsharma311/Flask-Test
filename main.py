9a5b24ba00ebfc8f4b1ff59b909986ce8a621fe9
kaushalsharma311 <38313525+kaushalsharma311@users.noreply.github.com>
2018-04-16 11:20:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,9 @@ def hello_world():
 <html>
     <head>
         <title>Dashboard</title>
-        <script>
-                function resizeIframe(obj) {
-                                              obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
-                }
-        </script>
     </head>
     <body>
-          <iframe height=600 width=1200 src="https://app.powerbi.com/view?r=eyJrIjoiNTIwZGM2ZjAtOGVmNC00YzIwLTk1MTctNWViMGRhMzBhODFmIiwidCI6ImRhNjdlZjFiLWNhNTktNGRiMi05YThjLWFhOGQ5NDYxN2ExNiIsImMiOjR9&$filter=Air_Monitoring_Stations/SITE_NAME+eq+'MON'" frameborder="0" allowFullScreen="true"></iframe>        
+          <iframe height=600 width=1200 src="https://app.powerbi.com/view?r=eyJrIjoiODM2YTE1ZGYtZmMzYS00YWMyLWFkNjYtNTliOTNkYjhkODRjIiwidCI6ImRhNjdlZjFiLWNhNTktNGRiMi05YThjLWFhOGQ5NDYxN2ExNiIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>        
         <form method="POST">
             <input class="btn" type="submit" value="submit">
         </form>
